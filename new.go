bf557066c5a4ce5e85d11570ed9aeb4edc5cba99
Rangi Lin <rangiltw@gmail.com>
2014-09-02 23:06:28 +0800

    add message

--- new.go
+++ new.go
@@ -11,11 +11,14 @@ func createBlogLayout() error {
 	if err != nil {
 		return fmt.Errorf("Unable to get file current file path")
 	}
+	fmt.Println("[GD] create _markdowns folder...")
 	if err := os.Mkdir(dir+"/_markdowns", os.ModePerm); err != nil {
 		return fmt.Errorf("Unable to create folder %s", dir+"/_markdowns")
 	}
+	fmt.Println("[GD] create _engine folder...")
 	if err := os.Mkdir(dir+"/_engine", os.ModePerm); err != nil {
 		return fmt.Errorf("Unable to create folder %s", dir+"/_engine")
 	}
+	fmt.Println("[GD] Done ! :)")
 	return nil
 }
