42570215b231010aca7ac4eba86b299da21f5168
Rangi Lin <rangiltw@gmail.com>
2014-09-15 23:01:48 +0800

    remove unused function

--- testutil.go
+++ testutil.go
@@ -3,7 +3,6 @@ package main
 import (
 	"io/ioutil"
 	"os"
-	"path/filepath"
 	"testing"
 )
 
@@ -12,15 +11,6 @@ func assertFilePathExist(t *testing.T, path string) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		t.Fatalf("%s should exist, but not.", path)
 	}
-}
-
-// fakeExecutionPath change current execution path to a temporary directory.
-func fakeExecutionPath(t *testing.T) {
-	path, err := ioutil.TempDir("", "glowing_dangerzone_test_")
-	if err != nil {
-		t.Fatalf("Unable to create temp dir: %s, error: %s", path, err)
-	}
-	os.Args[0] = filepath.Join(path, filepath.Base(os.Args[0]))
 }
 
 func createTmpFolder(t *testing.T) string {
