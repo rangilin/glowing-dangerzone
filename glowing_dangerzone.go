3118448d467c55029c50bce399408bec0a100f00
Rangi Lin <rangilin@otiga.com>
2015-01-06 17:01:31 +0800

    clean up

--- glowing_dangerzone.go
+++ glowing_dangerzone.go
@@ -7,35 +7,30 @@ import (
 )
 
 func main() {
-	if len(os.Args) <= 1 {
-		return
-	}
 
-	var dir string
-	var err error
-	dir, err = getCurrentDir()
-	if err != nil {
-		logErrorAndShutdown(err)
-	}
-
-	cmd := os.Args[1]
+	dir := getCurrentDir()
+	cmd := getSubCommand()
 	if cmd == "new" {
-		err = createBlogLayout(dir)
+		createBlogLayout(dir)
 	} else if cmd == "build" {
-		err = BlogBuilder{dir + "/blog"}.Build()
-	}
-
-	if err != nil {
-		logErrorAndShutdown(err)
+		BlogBuilder{dir + "/blog"}.Build()
+	} else {
+		log.Fatalf("unknown command %s", cmd)
 	}
 }
 
-func getCurrentDir() (dir string, err error) {
-	dir, err = filepath.Abs(filepath.Dir(os.Args[0]))
+func getSubCommand() (cmd string) {
+	cmd = ""
+	if len(os.Args) > 1 {
+		cmd = os.Args[1]
+	}
 	return
 }
 
-func logErrorAndShutdown(err error) {
-	log.Fatal(err)
-	os.Exit(1)
+func getCurrentDir() string {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	return dir
 }
