dcc7e911a55682c53ea2016ce8c615ce3a4b619b
rangilin <rangiltw@gmail.com>
2015-01-30 00:09:38 +0800

    use template inheritance by default

--- templates.go
+++ templates.go
@@ -1,7 +1,8 @@
 package main
 
 // BaseTemplateContent is HTML for base.tmpl
-const BaseTemplateContent = `<html>
+const BaseTemplateContent = `{{ define "base" }}
+<html>
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,18 +10,20 @@ const BaseTemplateContent = `<html>
     <title></title>
   </head>
   <body>
-    <div>{{.Content}}</div>
+    <div>{{ template "content" }}</div>
   </body>
 </html>
+{{ end }}
 `
 
 // IndexTemplateContent is HTML for index page
-const IndexTemplateContent = `<div>
-Index</div>
+const IndexTemplateContent = `{{ define "content" }}
+<div>Index</div>
+{{ end }}
 `
 
 // PostTemplateContent is HTML for post page
-const PostTemplateContent = `<div>
-  {{.Content}}
-</div>
+const PostTemplateContent = `{{ define "content"}}
+<div>{{.Content}}</div>
+{{ end }}
 `
