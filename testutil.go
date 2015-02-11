5ee43d9693a408696f952f8657dfb9775f3faa46
Rangi Lin <rangiltw@gmail.com>
2015-02-11 16:18:55 +0800

    move testing directory to specific tmp dir

--- testutil.go
+++ testutil.go
@@ -43,7 +43,12 @@ func assertFileContains(t *testing.T, path string, substr string) {
 
 // create a temporary folder for testing
 func createTmpFolder(t *testing.T) string {
-	dir, err := ioutil.TempDir("", "glowing_dangerzone_test_")
+	tmp := filepath.Join(os.TempDir(), "glowing_dangerzone_tmp")
+	if _, err := os.Stat(tmp); os.IsNotExist(err) {
+		os.Mkdir(tmp, os.ModePerm)
+	}
+
+	dir, err := ioutil.TempDir(tmp, "test_")
 	if err != nil {
 		t.Fatalf("Unable to create temp dir: %s, error: %s", dir, err)
 	}
