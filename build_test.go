a8a7f5c6331c9f5de84336549a7d1bfd05beec08
rangilin <rangiltw@gmail.com>
2015-04-04 17:13:41 +0800

    remove hardcoded name

--- build_test.go
+++ build_test.go
@@ -211,7 +211,7 @@ func TestGenerateRSSXML(t *testing.T) {
 	expect := `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
-    <title>Rangi Lin's Blog</title>
+    <title></title>
     <description></description>
     <link>http://localhost/</link>
 
--- templates.go
+++ templates.go
@@ -36,7 +36,7 @@ const PostTemplateContent = `{{ define "content"}}
 const FeedsXMLContent = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
-    <title>Rangi Lin's Blog</title>
+    <title></title>
     <description></description>
     <link>{{ .Conf.BaseUrl }}</link>
     {{ range .Posts }}
